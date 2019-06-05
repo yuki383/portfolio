4eb09aa32b2816f0e9bee7e3b9b496af7342253b
yuki <yuukikokubo.pg@gmail.com>
2019-06-05 15:54:39 +0900

    Worksのスクリーンの作成

---
+++ src/components/Main/WorksScreen/index.tsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+import ScreenTop from "../../utils/ScreenTop";
+
+const Works: React.FC = () => {
+
+  return(
+    <>
+      <ScreenTop title="Works" />
+    </>
+  )
+}
+
+export default Works;
--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import { Route } from "react-router-dom";
 import Profiles from "./Profile";
+import Works from "./WorksScreen";
 
 interface Props {
 
@@ -11,6 +12,7 @@ const HomeScreen: React.FC = () => {
   return (
     <main>
       <Route exact path="/" component={Profiles} />
+      <Route path="/works" component={Works} />
     </main>
   )
 
